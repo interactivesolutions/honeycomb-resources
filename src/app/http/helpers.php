3d00aa57a5d3aaee49f8d2cd4c72747064b276cc
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-02-19 17:13:03 +0200

    New function for YES NO values

--- src/app/http/helpers.php
+++ src/app/http/helpers.php
@@ -1,1 +1,9 @@
 <?php
+
+if (!function_exists ('formManagerYesNo')) {
+
+    function formManagerYesNo ()
+    {
+        return [['id' => '1', 'label' => 'Yes'], ['id' => '0', 'label' => 'No']];
+    }
+}
