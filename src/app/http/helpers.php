23c6927b2c17412ea18885f28ff33a50046b07e0
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-02-20 12:43:08 +0200

    moved function to core

--- src/app/http/helpers.php
+++ src/app/http/helpers.php
@@ -1,9 +1,1 @@
 <?php
-
-if (!function_exists ('formManagerYesNo')) {
-
-    function formManagerYesNo ()
-    {
-        return [['id' => '1', 'label' => 'Yes'], ['id' => '0', 'label' => 'No']];
-    }
-}
