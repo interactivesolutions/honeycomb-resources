da071bf04ee17e544c5db4d24c930e02be6063ee
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-02-20 14:32:36 +0200

    updated translations

--- src/resources/lang/en/resources_thumbs.php
+++ src/resources/lang/en/resources_thumbs.php
@@ -1,15 +1,15 @@
 <?php
 return [
-    'page_title'   => 'HCThumbs',
-    'count'        => 'Count',
-    'id'           => 'Id',
-    'created_at'   => 'Created at',
-    'updated_at'   => 'Updated at',
-    'deleted_at'   => 'Deleted at',
-    'name'         => 'Name',
-    'width'        => 'Width',
-    'height'       => 'Height',
-    'fit'          => 'Fit',
-    'aspect_ratio' => 'Aspect ratio',
+    'page_title' => 'Thumbnails configurations',
+    'count'      => 'Count',
+    'id'         => 'Id',
+    'created_at' => 'Created at',
+    'updated_at' => 'Updated at',
+    'deleted_at' => 'Deleted at',
+    'name'       => 'Name',
+    'width'      => 'Width',
+    'height'     => 'Height',
+    'fit'        => 'Fit',
+    'global'     => 'Global',
 
 ];
