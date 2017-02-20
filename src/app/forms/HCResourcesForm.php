a8ae7f94721bb267010785200726371d41fc75c4
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-02-20 09:46:44 +0200

    Updated form

--- src/app/forms/HCResourcesForm.php
+++ src/app/forms/HCResourcesForm.php
@@ -27,43 +27,46 @@ class HCResourcesForm
                     "type"  => "submit",
                 ],
             ],
-            'structure'  => [
-                [
-                    "type"            => "singleLine",
-                    "fieldID"         => "original_name",
-                    "label"           => trans ("HCResources::resources.original_name"),
-                    "required"        => 1,
-                    "requiredVisible" => 1,
-                ], [
-                    "type"            => "singleLine",
-                    "fieldID"         => "safe_name",
-                    "label"           => trans ("HCResources::resources.safe_name"),
-                    "required"        => 1,
-                    "requiredVisible" => 1,
-                ], [
-                    "type"            => "singleLine",
-                    "fieldID"         => "size",
-                    "label"           => trans ("HCResources::resources.size"),
-                    "required"        => 0,
-                    "requiredVisible" => 0,
-                ], [
-                    "type"            => "singleLine",
-                    "fieldID"         => "path",
-                    "label"           => trans ("HCResources::resources.path"),
-                    "required"        => 0,
-                    "requiredVisible" => 0,
-                ],
-            ],
+            'structure'  => [[
+                "type"            => "resource",
+                "fieldID"         => "resource",
+                "uploadURL"       => route("admin.api.resources"),
+                "viewURL"         => route("resource.get", ['/']),
+                "label"           => trans ("HCResources::resources.resource"),
+                "required"        => 1,
+                "requiredVisible" => 1,
+            ]],
         ];
-
-        if ($this->multiLanguage)
-            $form['availableLanguages'] = []; //TOTO implement honeycomb-languages package
 
         if (!$edit)
             return $form;
 
         //Make changes to edit form if needed
-        // $form['structure'][] = [];
+        $form['structure'] = array_merge ($form['structure'], [[
+            "type"            => "singleLine",
+            "fieldID"         => "original_name",
+            "label"           => trans ("HCResources::resources.original_name"),
+            "required"        => 1,
+            "requiredVisible" => 1,
+        ], [
+            "type"            => "singleLine",
+            "fieldID"         => "id",
+            "label"           => trans ("HCResources::resources.safe_name"),
+            "required"        => 1,
+            "requiredVisible" => 1,
+        ], [
+            "type"            => "singleLine",
+            "fieldID"         => "size",
+            "label"           => trans ("HCResources::resources.size"),
+            "required"        => 0,
+            "requiredVisible" => 0,
+        ], [
+            "type"            => "singleLine",
+            "fieldID"         => "path",
+            "label"           => trans ("HCResources::resources.path"),
+            "required"        => 0,
+            "requiredVisible" => 0,
+        ]]);
 
         return $form;
     }
