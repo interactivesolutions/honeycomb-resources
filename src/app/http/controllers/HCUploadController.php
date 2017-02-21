fa65d6dbab9e2e1e2e52bb63f74f2d979717ad31
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-02-21 18:17:16 +0200

    Added downloadAndSaveImage function

--- src/app/http/controllers/HCUploadController.php
+++ src/app/http/controllers/HCUploadController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Storage;
 use interactivesolutions\honeycombresources\models\HCResources;
+use phpDocumentor\Reflection\Types\Boolean;
 use Ramsey\Uuid\Uuid;
 
 class HCUploadController
@@ -28,10 +29,11 @@ class HCUploadController
      * Upload and insert new resource into database
      * Catch for errors and if is error throw error
      *
-     * @param $file
-     * @return array
+     * @param UploadedFile $file
+     * @param bool $full
+     * @return mixed
      */
-    public function upload (UploadedFile $file)
+    public function upload (UploadedFile $file, bool $full = null)
     {
         if (is_null ($file))
             return HCLog::info ('R-UPLOAD-001', trans ('resources::resources.errors.no_resource_selected'));
@@ -42,7 +44,7 @@ class HCUploadController
             $resource = $this->createResourceRecord ($file);
             $this->saveResourceInStorage ($resource, $file);
 
-            Artisan::call('hc:generate-thumbs', ['id' => $resource->id]);
+            Artisan::call ('hc:generate-thumbs', ['id' => $resource->id]);
         } catch (\Exception $e) {
             DB::rollback ();
 
@@ -54,6 +56,9 @@ class HCUploadController
         }
 
         DB::commit ();
+
+        if ($full)
+            return $resource->toArray();
 
         return [
             'id'  => $resource->id,
@@ -86,10 +91,10 @@ class HCUploadController
 
         $params['id'] = Uuid::uuid4 ();
         $params['original_name'] = $file->getClientOriginalName ();
-        $params['extension'] = '.' . $file->getClientOriginalExtension();
+        $params['extension'] = '.' . $file->getClientOriginalExtension ();
         $params['path'] = $this->uploadPath . $params['id'] . $params['extension'];
         $params['size'] = $file->getClientSize ();
-        $params['mime_type'] = $file->getClientMimeType();
+        $params['mime_type'] = $file->getClientMimeType ();
 
         return $params;
     }
@@ -129,5 +134,54 @@ class HCUploadController
         if (Storage::has ($path)) {
             Storage::delete ($path);
         }
+    }
+
+    /**
+     * Downloading and storing image in the system
+     *
+     * @param $imageURL
+     * @param bool $full - if set to true than return full resource data
+     * @return mixed
+     */
+    public function downloadAndSaveImage ($imageURL, bool $full = null)
+    {
+        $this->createFolder ('uploads/tmp');
+
+        $fileName = $this->getFileName ($imageURL);
+
+        if ($fileName && $fileName != '') {
+            $destination = storage_path ('app/uploads/tmp/' . $fileName);
+
+            // download resource to tmp folder with wget
+            echo (shell_exec (sprintf ('wget --quiet -O %s %s', escapeshellarg ($destination), escapeshellarg ($imageURL))));
+
+            if (!\File::exists ($destination)) {
+                return null;
+            }
+
+            $file = new UploadedFile($destination, $fileName, mime_content_type ($destination), filesize ($destination), null, true);
+
+            return $this->upload ($file, $full);
+        }
+
+        return null;
+    }
+
+    /**
+     * Retrieving file name
+     *
+     * @param $fileName
+     * @return mixed
+     */
+    protected function getFileName ($fileName)
+    {
+        if (!$fileName && filter_var ($fileName, FILTER_VALIDATE_URL) === false) {
+            return null;
+        }
+
+        $explodeFileURL = explode ('/', $fileName);
+        $fileName = end ($explodeFileURL);
+
+        return sanitizeString (pathinfo ($fileName, PATHINFO_FILENAME)) . '.' . pathinfo ($fileName, PATHINFO_EXTENSION);
     }
 }
