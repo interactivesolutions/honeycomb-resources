be3d77c4c26385c0caeaee79a257037d0f32e8c7
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-02-27 14:19:26 +0200

    Added helpers functions

--- src/app/http/helpers.php
+++ src/app/http/helpers.php
@@ -1,1 +1,92 @@
 <?php
+
+use Intervention\Image\Constraint;
+use Intervention\Image\ImageManagerStatic as Image;
+
+if (!function_exists ('generateResourceCacheLocation')) {
+
+    /**
+     * Generating resource cache location and name
+     *
+     * @param $id
+     * @param int|null $width
+     * @param int|null $height
+     * @param null $fit
+     * @return mixed
+     */
+    function generateResourceCacheLocation ($id, $width = 0, $height = 0, $fit = null)
+    {
+        $path = storage_path ('app/') . 'cache/' . str_replace ('-', '/', $id) . '/';
+
+        if (!is_dir ($path))
+            mkdir ($path, 0755, true);
+
+        $path .= $width . '_' . $height;
+
+        if ($fit)
+            $path .= '_fit';
+
+        return $path;
+    }
+
+    /**
+     * Generating resource public location and name
+     *
+     * @param $id
+     * @param int|null $width
+     * @param int|null $height
+     * @param null $fit
+     * @return mixed
+     */
+    function generateResourcePublicLocation ($id, $width = 0, $height = 0, $fit = null)
+    {
+        $path = public_path ('thumbs/') . str_replace ('-', '/', $id) . '/';
+
+        if (!is_dir ($path))
+            mkdir ($path, 0755, true);
+
+        $path .= $width . '_' . $height;
+
+        if ($fit)
+            $path .= '_fit';
+
+        return $path;
+    }
+}
+
+if (!function_exists ('createImage')) {
+    /**
+     * Creating image based on provided data
+     *
+     * @param $source
+     * @param $destination
+     * @param int $width
+     * @param int $height
+     * @param bool $fit
+     * @return bool
+     */
+    function createImage ($source, $destination, $width = 0, $height = 0, $fit = false)
+    {
+        if ($width == 0)
+            $width = null;
+
+        if ($height == 0)
+            $height = null;
+
+        $image = Image::make ($source);
+
+        if ($fit)
+            $image->fit ($width, $height, function (Constraint $constraint) {
+                $constraint->upsize ();
+            });
+        else
+            $image->resize ($width, $height, function (Constraint $constraint) {
+                $constraint->upsize ();
+                $constraint->aspectRatio ();
+            });
+
+        $image->save ($destination);
+
+        return true;
+    }
+}
