aec4755dce3c66f1bc2e7d12021616473a9bf93a
Liutauras Kavaliauskas <liutauraskav@gmail.com>
2017-03-02 14:33:42 +0200

    Updated downloadResource logics
    Added mime_type setting

--- src/app/http/controllers/HCUploadController.php
+++ src/app/http/controllers/HCUploadController.php
@@ -8,8 +8,6 @@ use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Storage;
 use interactivesolutions\honeycombresources\app\models\HCResources;
-use phpDocumentor\Reflection\Types\Boolean;
-use phpDocumentor\Reflection\Types\Resource;
 use Ramsey\Uuid\Uuid;
 
 class HCUploadController
@@ -28,9 +26,9 @@ class HCUploadController
      */
     private $resourceID;
 
-    public function __construct ()
+    public function __construct()
     {
-        $this->uploadPath = 'uploads/' . date ("Y-m-d") . DIRECTORY_SEPARATOR;
+        $this->uploadPath = 'uploads/' . date("Y-m-d") . DIRECTORY_SEPARATOR;
     }
 
     /**
@@ -41,39 +39,40 @@ class HCUploadController
      * @param bool $full
      * @param string $id
      * @return mixed
+     * @throws \Exception
      */
-    public function upload (UploadedFile $file, bool $full = null, string $id = null)
+    public function upload(UploadedFile $file, bool $full = null, string $id = null)
     {
-        if (is_null ($file))
-            return HCLog::info ('R-UPLOAD-001', trans ('resources::resources.errors.no_resource_selected'));
+        if (is_null($file))
+            return HCLog::info('R-UPLOAD-001', trans('resources::resources.errors.no_resource_selected'));
 
-        DB::beginTransaction ();
+        DB::beginTransaction();
 
         $this->resourceID = $id;
 
         try {
-            $resource = $this->createResourceRecord ($file);
-            $this->saveResourceInStorage ($resource, $file);
+            $resource = $this->createResourceRecord($file);
+            $this->saveResourceInStorage($resource, $file);
 
-            Artisan::call ('hc:generate-thumbs', ['id' => $resource->id]);
+            Artisan::call('hc:generate-thumbs', ['id' => $resource->id]);
         } catch (\Exception $e) {
-            DB::rollback ();
+            DB::rollback();
 
             if (isset($resource)) {
-                $this->removeImageFromStorage ($resource);
+                $this->removeImageFromStorage($resource);
             }
 
-            return HCLog::info ('R-UPLOAD-' . $e->getCode (), $e->getMessage ());
+            throw new \Exception($e);
         }
 
-        DB::commit ();
+        DB::commit();
 
         if ($full)
-            return $resource->toArray ();
+            return $resource->toArray();
 
         return [
             'id'  => $resource->id,
-            'url' => route ('resource.get', $resource->id),
+            'url' => route('resource.get', $resource->id),
         ];
     }
 
@@ -83,10 +82,10 @@ class HCUploadController
      * @param $file
      * @return HCResources
      */
-    protected function createResourceRecord (UploadedFile $file)
+    protected function createResourceRecord(UploadedFile $file)
     {
-        return HCResources::create (
-            $this->getFileParams ($file)
+        return HCResources::create(
+            $this->getFileParams($file)
         );
     }
 
@@ -96,20 +95,20 @@ class HCUploadController
      * @param $file
      * @return array
      */
-    public function getFileParams (UploadedFile $file)
+    public function getFileParams(UploadedFile $file)
     {
         $params = [];
 
         if ($this->resourceID)
             $params['id'] = $this->resourceID;
         else
-            $params['id'] = Uuid::uuid4 ();
+            $params['id'] = Uuid::uuid4();
 
-        $params['original_name'] = $file->getClientOriginalName ();
-        $params['extension'] = '.' . $file->getClientOriginalExtension ();
+        $params['original_name'] = $file->getClientOriginalName();
+        $params['extension'] = '.' . $file->getClientOriginalExtension();
         $params['path'] = $this->uploadPath . $params['id'] . $params['extension'];
-        $params['size'] = $file->getClientSize ();
-        $params['mime_type'] = $file->getClientMimeType ();
+        $params['size'] = $file->getClientSize();
+        $params['mime_type'] = $file->getClientMimeType();
 
         return $params;
     }
@@ -121,20 +120,20 @@ class HCUploadController
      * @param $file
      * @return mixed
      */
-    protected function saveResourceInStorage (HCResources $resource, UploadedFile $file)
+    protected function saveResourceInStorage(HCResources $resource, UploadedFile $file)
     {
-        $this->createFolder ($this->uploadPath);
-        $file->move (storage_path ('app/' . $this->uploadPath), $resource->id . '.' . $file->getClientOriginalExtension ());
+        $this->createFolder($this->uploadPath);
+        $file->move(storage_path('app/' . $this->uploadPath), $resource->id . '.' . $file->getClientOriginalExtension());
     }
 
     /**
      * Create folder
      * @param $path
      */
-    protected function createFolder (string $path)
+    protected function createFolder(string $path)
     {
-        if (!Storage::exists ($path))
-            Storage::makeDirectory ($path);
+        if (!Storage::exists($path))
+            Storage::makeDirectory($path);
     }
 
     /**
@@ -142,12 +141,12 @@ class HCUploadController
      *
      * @param $resource
      */
-    protected function removeImageFromStorage (HCResources $resource)
+    protected function removeImageFromStorage(HCResources $resource)
     {
         $path = $this->uploadPath . $resource->id;
 
-        if (Storage::has ($path)) {
-            Storage::delete ($path);
+        if (Storage::has($path)) {
+            Storage::delete($path);
         }
     }
 
@@ -157,26 +156,31 @@ class HCUploadController
      * @param string $source
      * @param bool $full - if set to true than return full resource data
      * @param string $id
+     * @param null|string $mime_type
      * @return mixed
      */
-    public function downloadResource (string $source, bool $full = null, string $id = null)
+    public function downloadResource(string $source, bool $full = null, string $id = null, string $mime_type = null)
     {
-        $this->createFolder ('uploads/tmp');
+        $this->createFolder('uploads/tmp');
 
-        $fileName = $this->getFileName ($source);
+        $fileName = $this->getFileName($source);
 
         if ($fileName && $fileName != '') {
-            $destination = storage_path ('app/uploads/tmp/' . $fileName);
 
-            file_put_contents ($destination, file_get_contents ($source));
+            $destination = storage_path('app/uploads/tmp/' . $fileName);
 
-            if (!\File::exists ($destination)) {
+            if (!$mime_type)
+                $mime_type = mime_content_type($destination);
+
+            file_put_contents($destination, file_get_contents($source));
+
+            if (!\File::exists($destination)) {
                 return null;
             }
 
-            $file = new UploadedFile($destination, $fileName, mime_content_type ($destination), filesize ($destination), null, true);
+            $file = new UploadedFile($destination, $fileName, $mime_type, filesize($destination), null, true);
 
-            return $this->upload ($file, $full, $id);
+            return $this->upload($file, $full, $id);
         }
 
         return null;
@@ -188,15 +192,15 @@ class HCUploadController
      * @param $fileName
      * @return mixed
      */
-    protected function getFileName (string $fileName)
+    protected function getFileName(string $fileName)
     {
-        if (!$fileName && filter_var ($fileName, FILTER_VALIDATE_URL) === false) {
+        if (!$fileName && filter_var($fileName, FILTER_VALIDATE_URL) === false) {
             return null;
         }
 
-        $explodeFileURL = explode ('/', $fileName);
-        $fileName = end ($explodeFileURL);
+        $explodeFileURL = explode('/', $fileName);
+        $fileName = end($explodeFileURL);
 
-        return sanitizeString (pathinfo ($fileName, PATHINFO_FILENAME)) . '.' . pathinfo ($fileName, PATHINFO_EXTENSION);
+        return sanitizeString(pathinfo($fileName, PATHINFO_FILENAME)) . '.' . pathinfo($fileName, PATHINFO_EXTENSION);
     }
 }
