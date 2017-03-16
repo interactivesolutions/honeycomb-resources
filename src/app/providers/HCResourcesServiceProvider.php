a5650670ad121c23441df8583bf979efcc46fce5
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-03-16 10:46:57 +0200

    Updated service provider to fit HCBaseServiceProvider

--- src/app/providers/HCResourcesServiceProvider.php
+++ src/app/providers/HCResourcesServiceProvider.php
@@ -2,12 +2,11 @@
 
 namespace interactivesolutions\honeycombresources\app\providers;
 
-use Illuminate\Support\ServiceProvider;
-//use interactivesolutions\honeycombscripts\app\console\commands\HCGenerateThumbs;
+use interactivesolutions\honeycombcore\providers\HCBaseServiceProvider;
 use interactivesolutions\honeycombresources\app\console\commands\HCGenerateThumbs;
 use Intervention\Image\ImageServiceProvider;
 
-class HCResourcesServiceProvider extends ServiceProvider
+class HCResourcesServiceProvider extends HCBaseServiceProvider
 {
     /**
      * Register commands
@@ -21,81 +20,9 @@ class HCResourcesServiceProvider extends ServiceProvider
     protected $namespace = 'interactivesolutions\honeycombresources\app\http\controllers';
 
     /**
-     * Bootstrap the application services.
-     */
-    public function boot ()
-    {
-        // register artisan commands
-        $this->commands ($this->commands);
-
-        // loading views
-        $this->loadViewsFrom (__DIR__ . '/../../resources/views', 'HCResources');
-
-        // loading translations
-        $this->loadTranslationsFrom (__DIR__ . '/../../resources/lang', 'HCResources');
-
-        // registering elements to publish
-        $this->registerPublishElements ();
-
-        // registering helpers
-        $this->registerHelpers ();
-
-        // registering routes
-        $this->registerRoutes ();
-
-        $this->registerProviders();
-    }
-
-    /**
-     * Register helper function
-     */
-    private function registerHelpers ()
-    {
-        $filePath = __DIR__ . '/../http/helpers.php';
-
-        if (\File::isFile ($filePath))
-            require_once $filePath;
-    }
-
-    /**
-     *  Registering all vendor items which needs to be published
-     */
-    private function registerPublishElements ()
-    {
-        $directory = __DIR__ . '/../../database/migrations/';
-
-        // Publish your migrations
-        if (file_exists ($directory))
-            $this->publishes ([
-                __DIR__ . '/../../database/migrations/' => database_path ('/migrations'),
-            ], 'migrations');
-
-        $directory = __DIR__ . '/../public';
-
-        // Publishing assets
-        if (file_exists ($directory))
-            $this->publishes ([
-                __DIR__ . '/../public' => public_path ('honeycomb'),
-            ], 'public');
-    }
-
-    /**
-     * Registering routes
-     */
-    private function registerRoutes ()
-    {
-        $filePath = __DIR__ . '/../../app/honeycomb/routes.php';
-
-        if (file_exists($filePath))
-            \Route::group (['namespace' => $this->namespace], function ($router) use ($filePath) {
-                require $filePath;
-            });
-    }
-
-    /**
      * Registering external providers
      */
-    private function registerProviders ()
+    protected function registerProviders ()
     {
         $this->app->register(ImageServiceProvider::class);
     }
