7896ee1a90c2e8d89bd968957a50756f60b6a817
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-06-14 22:50:12 +0300

    Fixed list controller

--- src/app/routes/admin/01_routes.resources.thumbs.php
+++ src/app/routes/admin/01_routes.resources.thumbs.php
@@ -10,7 +10,7 @@ Route::group(['prefix' => config('hc.admin_url'), 'middleware' => ['web', 'auth'
         Route::post('/', ['middleware' => ['acl:interactivesolutions_honeycomb_resources_resources_thumbs_create'], 'uses' => 'resources\\HCThumbsController@apiStore']);
         Route::delete('/', ['middleware' => ['acl:interactivesolutions_honeycomb_resources_resources_thumbs_delete'], 'uses' => 'resources\\HCThumbsController@apiDestroy']);
 
-        Route::get('list', ['as' => 'admin.api.resources.thumbs.list', 'middleware' => ['acl:interactivesolutions_honeycomb_resources_resources_thumbs_list'], 'uses' => 'regions\\HCThumbsController@apiIndex']);
+        Route::get('list', ['as' => 'admin.api.resources.thumbs.list', 'middleware' => ['acl:interactivesolutions_honeycomb_resources_resources_thumbs_list'], 'uses' => 'resources\\HCThumbsController@apiIndex']);
         Route::post('restore', ['as' => 'admin.api.resources.thumbs.restore', 'middleware' => ['acl:interactivesolutions_honeycomb_resources_resources_thumbs_update'], 'uses' => 'resources\\HCThumbsController@apiRestore']);
         Route::post('merge', ['as' => 'admin.api.resources.thumbs.merge', 'middleware' => ['acl:interactivesolutions_honeycomb_resources_resources_thumbs_create', 'acl:interactivesolutions_honeycomb_resources_resources_thumbs_update'], 'uses' => 'resources\\HCThumbsController@apiMerge']);
         Route::delete('force', ['as' => 'admin.api.resources.thumbs.force.multi', 'middleware' => ['acl:interactivesolutions_honeycomb_resources_resources_thumbs_force_delete'], 'uses' => 'resources\\HCThumbsController@apiForceDelete']);
